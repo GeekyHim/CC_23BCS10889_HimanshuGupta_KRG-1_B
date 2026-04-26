d6aae338ad06524a76c867e0088a70c7f3717ff2
Himanshu Gupta <156525961+GeekyHim@users.noreply.github.com>
2026-04-26 22:29:48 +0530

    exp10leftfiles

---
+++ Experiment 10/EXP10.java
@@ -0,0 +1,78 @@
+import java.util.*;
+
+public class EXP10 {
+
+    static class Pair {
+        int val, index;
+
+        Pair(int val, int index) {
+            this.val = val;
+            this.index = index;
+        }
+    }
+
+    public static List<Integer> countSmaller(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        Pair[] arr = new Pair[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = new Pair(nums[i], i);
+        }
+
+        mergeSort(arr, 0, n - 1, result);
+
+        List<Integer> res = new ArrayList<>();
+        for (int count : result) {
+            res.add(count);
+        }
+        return res;
+    }
+
+    private static void mergeSort(Pair[] arr, int left, int right, int[] result) {
+        if (left >= right) return;
+
+        int mid = (left + right) / 2;
+        mergeSort(arr, left, mid, result);
+        mergeSort(arr, mid + 1, right, result);
+        merge(arr, left, mid, right, result);
+    }
+
+    private static void merge(Pair[] arr, int left, int mid, int right, int[] result) {
+        List<Pair> temp = new ArrayList<>();
+        int i = left, j = mid + 1;
+        int rightCount = 0;
+
+        while (i <= mid && j <= right) {
+            if (arr[j].val < arr[i].val) {
+                temp.add(arr[j]);
+                rightCount++;
+                j++;
+            } else {
+                result[arr[i].index] += rightCount;
+                temp.add(arr[i]);
+                i++;
+            }
+        }
+
+        while (i <= mid) {
+            result[arr[i].index] += rightCount;
+            temp.add(arr[i]);
+            i++;
+        }
+
+        while (j <= right) {
+            temp.add(arr[j]);
+            j++;
+        }
+
+        for (int k = left; k <= right; k++) {
+            arr[k] = temp.get(k - left);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {5, 2, 6, 1};
+        System.out.println(countSmaller(nums));
+    }
+}
